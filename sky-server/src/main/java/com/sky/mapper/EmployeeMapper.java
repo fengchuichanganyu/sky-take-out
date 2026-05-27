0ee8b282503a8d8211f58d7ac3479415017293f1
giteestudyroom <1132980470@qq.com>
2026-05-27 17:36:53 +0800

    编辑员工信息业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -32,6 +32,15 @@ public interface EmployeeMapper {
     void insert(Employee employee);
 
     /**
+     * 根据id查询员工
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
+
+    /**
      * 分页查询员工
      *
      * @param name 员工姓名
